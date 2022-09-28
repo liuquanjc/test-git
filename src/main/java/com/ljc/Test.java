f14c1598ed63f11ce158f3c87afeb2ab589d26d7
liuquanjc <112446947@qq.com>
2022-09-28 21:19:09 +0800

    Initial commit

--- src/main/java/com/ljc/Test.java
+++ src/main/java/com/ljc/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args){
         System.out.println("hello java");
         System.out.println("hello java1");
-        System.out.println("hello java2");
+        System.out.println("hello java3");
     }
 }
